9d23347c2ff27c6d9ad525d12e4a18a96b7345f7
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-05 16:48:14 +0000

    fixed check cycle logic

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -2,20 +2,16 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *current, *tortoise, *hare;
+	listint_t *tortoise, *hare;
 
-	if (list == NULL)
-		return (0);
-	current = list;
 	tortoise = list;
 	hare = list;
-	while (current->next->next!= NULL)
+	while (hare != NULL && hare->next!= NULL)
 	{
-		tortoise = current->next;
-		hare = current->next->next;
+		tortoise = tortoise->next;
+		hare = hare->next->next;
 		if (hare == tortoise)
 			return (1);
-		current = current->next;
 	}
 	return (0);
 }
