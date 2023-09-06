e38aba055a1a2655abe11d4f8acc44ea46b19a40
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-06 00:01:37 +0000

    task 11

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def pow(a, b):
+    product = 1
+    while (b != 0):
+        if (b > 0):
+            product *= a
+            b = b - 1
+        else:
+            product /= a
+            b = b + 1
+    return product
