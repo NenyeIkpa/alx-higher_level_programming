953e1d21a1a44583a91b5a88b50394169371098e
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-07 18:15:52 +0000

    task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,24 +2,25 @@
 import sys
 from calculator_1 import add, sub, mul, div
 
-argv = sys.argv
-arglen = len(argv)
+if __name__ == "__main__":
+    argv = sys.argv
+    arglen = len(argv)
 
-if (arglen != 4):
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
-else:
-    a = argv[1]
-    b = argv[3]
-    operator = argv[2]
-    if (operator == "+"):
-        print("{} + {} = {}".format(a, b, add(int(a), int(b))))
-    elif (operator == "-"):
-        print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
-    elif (operator == "*"):
-        print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
-    elif (operator == "/"):
-        print("{} / {} = {}".format(a, b, div(int(a), int(b))))
-    else:
-        print("Unknown operator. Available operators: +, -, * and /")
+    if (arglen != 4):
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+    else:
+        a = argv[1]
+        b = argv[3]
+        operator = argv[2]
+        if (operator == "+"):
+            print("{} + {} = {}".format(a, b, add(int(a), int(b))))
+        elif (operator == "-"):
+            print("{} - {} = {}".format(a, b, sub(int(a), int(b))))
+        elif (operator == "*"):
+            print("{} * {} = {}".format(a, b, mul(int(a), int(b))))
+        elif (operator == "/"):
+            print("{} / {} = {}".format(a, b, div(int(a), int(b))))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
