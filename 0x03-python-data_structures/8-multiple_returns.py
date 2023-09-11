1277de3a75d90ad57d28ee9e6b5a26793752d7c1
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-11 18:18:14 +0000

    task 8 edit

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    senlen = len(sentence)
-
     if (sentence == ""):
-        sentence[0] = None
-    return (senlen, sentence[0])
+        return (0, None)
+    return (len(sentence), sentence[0])
