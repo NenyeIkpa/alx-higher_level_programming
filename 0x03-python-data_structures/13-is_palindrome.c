78cd331c0a99f4fcaeb38cfa43f50239531b6593
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-12 08:54:32 +0000

    task 13 edit

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -14,36 +14,27 @@ void delete_list(listint_t *h)
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *h = NULL, *node = NULL, *curr = NULL;
+	listint_t *h = NULL, *node = NULL, *next = NULL, *prev = NULL;
 
 	if (head == NULL || *head == NULL)
 	       return (1);
-	curr = *head;
-	while (curr != NULL)
-	{
-		node = malloc(sizeof(listint_t));
-		if (node == NULL)
-			return (0);
-		node->n = curr->n;
-		node->next = NULL;
-		if (h == NULL)
-			h = node;
-		else
-		{
-			node->next = h;
-			h = node;
-		}
-		curr = curr->next;
+	node = *head;
+	while (node)
+	{		
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
 	}
-	curr = *head;
-	while ( curr != NULL && h != NULL)
+	h = prev;
+	while ( *head != NULL && h != NULL)
 	{
-		if (curr->n != h->n)
+		if ((*head)->n != h->n)
 		{
 			delete_list(h);
 			return (0);
 		}
-		curr = curr->next;
+		*head = (*head)->next;
 		h = h->next;
 	}
 	delete_list(h);
