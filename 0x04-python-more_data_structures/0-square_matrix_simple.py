6aebc8ae68b09d3a01a2914bd8929c0ed37e4d33
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-14 11:12:30 +0000

    task 5, edited long line in task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     # create and return a new matrix with squared of values of matrix
-    # new_matrix =  [list(map(lambda num: num ** 2, sublist)) for sublist in matrix
+    """ new_matrix =
+    [list(map(lambda num: num ** 2, sublist)) for sublist in matrix"""
     new_matrix = []
     for listed in matrix:
         new_list = []
