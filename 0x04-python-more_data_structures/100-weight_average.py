231562f3362278a97eadcd6554706b8ae774367a
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-15 23:08:07 +0000

    task 100

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/ur/bin/python3
+def weight_average(my_list=[]):
+    if (my_list == []):
+        return 0
+    total = 0
+    res = 0
+    for i in my_list:
+        total += i[0] * i[1]
+        res += i[1]
+    return total/res
