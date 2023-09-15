639576aad6100b9c5d420440744de7ca8cbe67d3
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-15 14:56:41 +0000

    task 10 edit

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,6 +2,6 @@
 def best_score(a_dictionary):
     if (a_dictionary is None or a_dictionary == {}):
         return None
-    sorted_dict = sorted(a_dictionary.items(), key=lambda item: item[1])
+    sorted_dict = sorted(a_dictionary.items())
     max_value = sorted_dict.pop()
     return (max_value)
