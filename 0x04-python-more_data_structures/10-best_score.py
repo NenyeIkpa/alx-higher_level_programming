47cf043a77c07835e28a74a6fa752c23db5dadbf
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-15 14:46:46 +0000

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if (a_dictionary == None):
+    if (a_dictionary is None):
         return None
     sorted_dict = sorted(a_dictionary)
-    last_idx = sorted_dict.popItem()
-    print(last_idx)
-    return(last_idx)
+    max_value = sorted_dict.pop()
+    return (max_value)
