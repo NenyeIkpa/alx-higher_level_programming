72764936f23e495ccace5917458829b8414b58e4
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-25 22:37:25 +0000

    task 0 edit

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,9 +4,9 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
+            print("{}".format(my_list[i]), end="")
             count += 1
-            print("{}".format(my_list[i]), end="" if i < x - 1 else "\n")
-        except (IndexError, TypeError):
-            print()
-            return count - 1
+        except IndexError:
+            continue
+    print()
     return count
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 
 def safe_function(fct, *args):
     try:
         return fct(args)
     except (Exception):
-        Exception("Unknown format code 'd' for object of type 'str'")
-        return 0
+        raise Exception
+        return None
