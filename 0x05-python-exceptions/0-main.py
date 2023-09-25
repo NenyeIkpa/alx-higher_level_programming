976d1f7d2b2d35891fd3fd5db1fa23171a4311a0
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-25 13:44:15 +0000

    task 0 edit 1

--- 0x05-python-exceptions/0-main.py
+++ 0x05-python-exceptions/0-main.py
@@ -9,3 +9,8 @@ nb_print = safe_print_list(my_list, len(my_list))
 print("nb_print: {:d}".format(nb_print))
 nb_print = safe_print_list(my_list, len(my_list) + 2)
 print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, -1)
+print("nb_print: {:d}".format(nb_print))
+my_list = []
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,8 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
+    if (my_list == []):
+        return 0
     for i in range(x):
         try:
             count += 1
