f5751681d986b71773c6003f64384df98a706096
NenyeIkpa <nenyeikpa@gmail.com>
2023-09-26 23:02:48 +0000

    task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Creates a class named Square with attributes"""
+
+
+class Square:
+    """attributes include a private field; size and an init method"""
+
+    __size = None
+
+    def __init__(self, size=0):
+        """initialization function"""
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
