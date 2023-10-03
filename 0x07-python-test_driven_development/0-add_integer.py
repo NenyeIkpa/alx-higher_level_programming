5bb822ef9fc8837a36bfcaca8c8650a207244225
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-03 14:02:18 +0000

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Calculates the sum of two numbers
+    numbers can be of type int or float
+    otherwise, program will raise a TypeError
+"""
+
+def add_integer(a, b=98):
+    """checks if  a and b are of type int or float,
+    returns sum of a and b if true
+    else raises TypeError"""
+    if type(a) is None:
+        raise SyntaxError("a must be an integer")
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
