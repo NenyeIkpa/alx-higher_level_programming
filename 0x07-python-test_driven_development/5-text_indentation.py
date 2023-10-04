6269e78576a1ae037c16f45fe3f7caaf512ae9bc
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-04 04:50:22 +0000

    task 5 update

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -9,12 +9,13 @@ def text_indentation(text):
         raise TypeError("text must be a string")
     skip = 0
 
-    for char in text:
-        if char == "." or char == "?" or char == ":":
-            print(char, end="\n\n")
-            skip = 1
-        elif (char == " " or char == "\t") and skip == 1:
-            pass
-        else:
-            print(char, end="")
-            skip = 0
+    for i in range(len(text)):
+        if text[i] != " " or (i > 0 and text[i - 1] != " "):
+            if text[i] == "." or text[i] == "?" or text[i] == ":":
+                print(text[i], end="\n\n")
+                skip = 1
+            elif (text[i] == " " or text[i] == "\t") and skip == 1:
+                pass
+            else:
+                print(text[i], end="")
+                skip = 0
