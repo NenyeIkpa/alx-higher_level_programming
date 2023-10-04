9a7ee91d114a049817b192087018fe7b02780682
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-04 12:30:37 +0000

    task 3 update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -5,10 +5,10 @@
 
 def say_my_name(first_name="", last_name=""):
     """confirm parameters are strings and print name"""
-    if not isinstance(first_name, str) or type(first_name) is None:
+    if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
-    if not isinstance(last_name, str) or type(last_name) is None:
+    if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     print("My name is {:s} {:s}".format(first_name, last_name))
