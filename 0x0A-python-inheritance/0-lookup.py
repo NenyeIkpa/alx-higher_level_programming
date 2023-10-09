696e32500cbeffbeb0a56c7a16610ca5bd1cd9ce
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-09 20:30:29 +0000

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    How to display the list of available attributes
+    and methods of an object
+"""
+
+
+def lookup(obj):
+    """ returns the attributes and methods of obj using dir """
+    return dir(obj)
