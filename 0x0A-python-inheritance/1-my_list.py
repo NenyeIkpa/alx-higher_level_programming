5212a91b3c23976672251ad354fb12fa7a88d2f5
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-09 21:20:38 +0000

    task 1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+    creates a child class from a parent class
+"""
+
+
+class MyList(list):
+    """
+        MyList class inherits from the class, list
+        and therefore a sub or child class to the base
+        or parent class
+    """
+
+    def print_sorted(self):
+        new_list = sorted(self)
+        print(new_list)
