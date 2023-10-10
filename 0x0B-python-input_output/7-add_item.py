ea22f386dd505db2e27f887c602e02e78970a2e0
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-10 13:31:53 +0000

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+"""
+    script that adds all arguments to a Python list,
+    and then save them to a file
+"""
+import sys
+import os
+
+""" import required files """
+save_to = __import__('5-save_to_json_file').save_to_json_file
+load_from = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+"""
+    check if filename already exists and
+    load its contents to the python list, my list
+"""
+if os.path.exists(filename):
+    mylist = load_from(filename)
+else:
+    mylist = []
+
+""" when command line args are available, append them to the python list """
+if len(sys.argv) > 1:
+    for i in range(1, len(sys.argv)):
+        mylist.append(sys.argv[i])
+save_to(mylist, filename)
+load_from(filename)
