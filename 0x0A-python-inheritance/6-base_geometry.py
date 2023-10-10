772d63df73e2e50f870d5b02edcad8f46afa4c67
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-10 00:33:38 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""
+    creates a class
+"""
+
+
+class BaseGeometry:
+    """ a class named BaseGeometry """
+    def area(self):
+        return Exception("area() is not implemented")
