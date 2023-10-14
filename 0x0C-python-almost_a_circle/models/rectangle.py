c853ae9ad0f99d51bd06389de2500c19aaa917e5
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-14 01:17:28 +0000

    task 9

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -113,7 +113,7 @@ class Rectangle(Base):
         return "[Rectangle] ({:d}) {:d}/{:d} - {:d}/{:d}".format(
                 self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
             updates class attribute using
             no-keyword argument
@@ -121,27 +121,56 @@ class Rectangle(Base):
         if args:
             if len(args) >= 1:
                 self.id = args[0]
-                if len(args) >= 2:
-                    if type(args[1]) is not int:
+            if len(args) >= 2:
+                if type(args[1]) is not int:
+                    raise TypeError("width must be an integer")
+                if args[1] <= 0:
+                    raise ValueError("width must be > 0")
+                self.__width = args[1]
+            if len(args) >= 3:
+                if type(args[2]) is not int:
+                    raise TypeError("height must be an integer")
+                if args[2] <= 0:
+                    raise ValueError("height must be > 0")
+                self.__height = args[2]
+            if len(args) >= 4:
+                if type(args[3]) is not int:
+                    raise TypeError("x must be an integer")
+                if args[3] < 0:
+                    raise ValueError("x must be >= 0")
+                self.__x = args[3]
+            if len(args) >= 5:
+                if type(args[4]) is not int:
+                    raise TypeError("y must be an integer")
+                if args[4] < 0:
+                    raise ValueError("y must be >= 0")
+                self.__y = args[4]
+        else:
+            arg_list = ["id", "width", "height", "x", "y"]
+            for key, value in kwargs.items():
+                if key == arg_list[0]:
+                    self.__id = value
+                if key == arg_list[1]:
+                    if type(value) is not int:
                         raise TypeError("width must be an integer")
-                    if args[1] <= 0:
-                        raise ValueError("width must be >= 0")
-                    self.__width = args[1]
-                if len(args) >= 3:
-                    if type(args[2]) is not int:
+                    if value <= 0:
+                        raise ValueError("width must be > 0")
+                    self.__width = value
+                if key == arg_list[2]:
+                    if type(value) is not int:
                         raise TypeError("height must be an integer")
-                    if args[2] <= 0:
+                    if value <= 0:
                         raise ValueError("height must be > 0")
-                    self.__height = args[2]
-                if len(args) >= 4:
-                    if type(args[3]) is not int:
+                    self.__height = value
+                if key == arg_list[3]:
+                    if type(value) is not int:
                         raise TypeError("x must be an integer")
-                    if args[3] < 0:
-                        raise ValueError("x must be > 0")
-                    self.__x = args[3]
-                if len(args) >= 5:
-                    if type(args[4]) is not int:
+                    if value < 0:
+                        raise ValueError("x must be >= 0")
+                    self.__x = value
+                if key == arg_list[4]:
+                    if type(value) is not int:
                         raise TypeError("y must be an integer")
-                    if args[4] < 0:
-                        raise ValueError("y must be > 0")
-                    self.__y = args[4]
+                    if value < 0:
+                        raise ValueError("y must be >= 0")
+                    self.__y = value
