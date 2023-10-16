a7933b47f3435941561ebda120600d4406f13363
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-16 20:56:14 +0000

    task 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -16,6 +16,20 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size:
+        """ get method for size attribute  """
+        return self.__width
+
+    @size.setter
+    def size(self, value):
+        """ set method for size attribute """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = size
+
     def __str__(self):
         """ returns a string representation of class attributes """
         return "[Square] ({}) {}/{} - {}".format(
