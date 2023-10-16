5c99b77dd93457164ea940fb8a9e96f82a2c15fe
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-16 18:30:57 +0000

    test cases for rectangle class

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+    unit tests for Base class
+"""
+
+
+import unittest
+
+from models.base import Base
+
+class TestBase(unittest.TestCase):
+    """
+        holds all the test cases for the Base class
+    """
+    def test_init(self):
+        """
+            tests cases for attributes and methods of the base class
+        """
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+
+        b2 = Base (10)
+        self.assertEqual(b2.id, 10)
+
+
+        b3 = Base()
+        self.assertEqual(b3.id, 2)
+
+        b4 = Base(-5)
+        self.assertEqual(b4.id, -5)
+
+        b5 = Base(100)
+        self.assertEqual(b5.id, 100)
+
+    if __name__ == "__main__":
+        unittest.main()
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+"""
+    unit tests for Rectangle class
+"""
+
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestRectangle(unittest.TestCase):
+    """ 
+        tests cases for the properties and methods
+        of the rectangle class
+    """
+
+    def test_init_all_args(self):
+        """
+            tests the initialization function of Rectangle
+        """
+        r1 = Rectangle(10, 5, 3, 4, 12)
+        self.assertEqual(r1.width, 10)
+        self.assertEqual(r1.height, 5)
+        self.assertEqual(r1.x, 3)
+        self.assertEqual(r1.y, 4)
+        self.assertEqual(r1.id, 12)
+        self.assertEqual(r1.area(), 50)
+
+    def test_init_no_args(self):
+        """ no args passed to class instantiation """
+        with self.assertRaises(TypeError):
+            Rectangle()
+
+    def test_init_neg_values(self):
+        """ negative values passed as args of width and height """
+        with self.assertRaises(ValueError):
+            Rectangle(-7, -3)
+
+        """ negative values passed to x and y """
+        with self.assertRaises(ValueError):
+            Rectangle(2, 3, -3, -2)
+
