56f35f43b3321984178f0de9acd73bb5f9546a6e
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-17 09:25:58 +0000

    added more test cases to rectangle

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -26,16 +26,48 @@ class TestRectangle(unittest.TestCase):
         self.assertEqual(r1.id, 12)
         self.assertEqual(r1.area(), 50)
 
+    def test_init_width_varied_args(self):
+        """ initializes instances of a Rectangle with variable number of arguments"""
+        r2 = Rectangle(2,7)
+        self.assertEqual(r2.width, 2)
+        self.assertEqual(r2.height, 7)
+        self.assertEqual(r2.x, 0)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.area(), 14)
+
+        r3 = Rectangle(5,6,7)
+        self.assertEqual(r3.width, 5)
+        self.assertEqual(r3.height, 6)
+        self.assertEqual(r3.x, 7)
+        self.assertEqual(r3.y, 0)
+        self.assertEqual(r3.area(), 30)
+
+        r4 = Rectangle(5,6,7, 8)
+        self.assertEqual(r4.width, 5)
+        self.assertEqual(r4.height, 6)
+        self.assertEqual(r4.x, 7)
+        self.assertEqual(r4.y, 8)
+        self.assertEqual(r4.area(), 30)
+
+
     def test_init_no_args(self):
         """ no args passed to class instantiation """
         with self.assertRaises(TypeError):
             Rectangle()
 
     def test_init_neg_values(self):
-        """ negative values passed as args of width and height """
+        """ negative value passed as width """
         with self.assertRaises(ValueError):
-            Rectangle(-7, -3)
+            Rectangle(-7, 3)
 
-        """ negative values passed to x and y """
+        """ negative value passed as height """
         with self.assertRaises(ValueError):
-            Rectangle(2, 3, -3, -2)
+            Rectangle(7, -3)
+
+        """ negative value passed to x """
+        with self.assertRaises(ValueError):
+            Rectangle(2, 3, -3, 2)
+
+        """ negative value passed to y """
+        with self.assertRaises(ValueError):
+            Rectangle(2, 3, 3, -2)
