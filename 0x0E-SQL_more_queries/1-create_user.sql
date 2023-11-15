336f3d0b608c6788df5a9efc28c19b516e8bfe52
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-15 13:27:23 +0000

    task 1

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- creates a user
+-- does not fail if user exists
+-- user has all privileges granted
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
