b95d8f0bf5cfdae39d97f2ef7d1cc38cf5a648fe
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-16 12:56:46 +0000

    task 19-review

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -3,7 +3,7 @@
 -- ordered by temperature (descending)
 SELECT city, AVG(value) as avg_temp
 FROM temperatures
-WHERE month = 7 AND 8
+WHERE month = 7 OR month = 8
 GROUP BY city
 ORDER BY avg_temp DESC
 LIMIT 3;
