d4adf60bc49224372b74b789ff53715d95ee91ab
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-16 03:42:29 +0000

    task 15

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,11 @@
+-- Lists all comedy shows in the database hbtn_0d_tvshows.
+-- Records are ordered by descending show title.
+SELECT ts.title
+FROM tv_shows AS ts
+INNER JOIN tv_show_genres AS tsg
+ON ts.id = tsg.show_id
+
+INNER JOIN tv_genres AS tg
+ON tg.id = tsg.genre_id
+WHERE tg.name = "Comedy"
+ORDER BY ts.title;
