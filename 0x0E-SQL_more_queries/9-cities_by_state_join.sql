4121f25d25857645143fef59b34022a5234e2e9e
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-16 02:43:50 +0000

    task 9

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,3 @@
+-- lists all cities contained in database hbtn_0d_usa
+-- each record displays cities.id - cities.name - states.name
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON cities.state_id = states.id;
