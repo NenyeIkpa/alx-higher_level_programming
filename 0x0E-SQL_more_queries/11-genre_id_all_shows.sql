1a1e6582a499fa9c33347c40c74d1360390d69bf
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-16 11:36:34 +0000

    corrections to task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,7 +1,6 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
-SELECT
-ts.title, tsg.genre_id
+SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
 LEFT JOIN tv_show_genres as tsg ON ts.id = tsg.show_id
 ORDER BY ts.title ASC, tsg.genre_id ASC;
