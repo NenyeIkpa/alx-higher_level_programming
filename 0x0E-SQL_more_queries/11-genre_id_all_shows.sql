7389983e458dc43d424b9cf27d54222d3cdc25d1
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-17 07:31:10 +0000

    resolved as to AS - task 11

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,9 +1,8 @@
 -- lists all shows contained in hbtn_0d_tvshows
 -- Displays NULL for shows without genre
--- Results must be sorted in ascending order
--- order by tv_shows.title as ts.title and tv_show_genres.genre_id as tsg.genre_id
+-- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
 SELECT ts.title, tsg.genre_id
 FROM tv_shows AS ts
-LEFT JOIN tv_show_genres as tsg
+LEFT JOIN tv_show_genres AS tsg
 ON ts.id = tsg.show_id
 ORDER BY ts.title, tsg.genre_id;
