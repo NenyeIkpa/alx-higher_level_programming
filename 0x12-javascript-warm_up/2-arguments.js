f31336d57401857ca61006a6c197e55dd600406c
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-12 02:40:25 +0000

    task 2 update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,3 @@
-#!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length === 2) console.log('No argument');
-else if (argv.length === 3) console.log('Argument found'); else console.log('Arguments found');
+#!/usr/bin/node;
+if (process.argv.length === 2) console.log('No argument');
+else if (process.argv.length === 3) console.log('Argument found'); else console.log('Arguments found');
