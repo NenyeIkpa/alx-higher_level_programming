974880dda4f9ca0de3de2f7a977172a6ef0e12e5
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-13 02:15:16 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,11 +7,10 @@ class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c) {
-      for (let i = 0; i < this.height; i++) {
-        console.log(c.repeat(this.width));
-      }
-    } else super.print();
+    if (!c) c = 'X';
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
   }
 }
 
