06b3e8516476c954a0e58c3445f94807d5e36277
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-18 11:33:48 +0000

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+""" Search for a state  module """
+
+import sys
+import MySQLdb
+
+
+def list_states(user, password, db, state):
+    """list all states using MySQLdb """
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=user,
+            passwd=password,
+            db=db)
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name \
+            LIKE BINARY %s ORDER BY id ASC"
+    cur.execute(query, (state,))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    db = sys.argv[3]
+    state = sys.argv[4]
+    list_states(user, password, db, state)
