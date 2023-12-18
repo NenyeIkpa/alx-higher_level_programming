8665386a81ffe0d44a656c6c6b54557fc46b2cce
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-18 10:06:24 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-""" Select states module """
+""" Search for a state  module """
 
 import sys
 import MySQLdb
 
 
-def list_states(user, password, db, state_name_to_search):
+def list_states(user, password, db, state):
     """list all states using MySQLdb """
     db = MySQLdb.connect(
             host='localhost',
@@ -15,12 +15,11 @@ def list_states(user, password, db, state_name_to_search):
             passwd=password,
             db=db)
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' \
-            ORDER BY id ASC".format(state_name_to_search))
+    cur.execute("SELECT * FROM states WHERE name \
+            LIKE BINARY '{}' ORDER BY id ASC".format(state))
     states = cur.fetchall()
     for state in states:
-        if state is state_name_to_search:
-            print(state)
+        print(state)
     cur.close()
     db.close()
 
@@ -29,5 +28,5 @@ if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
     db = sys.argv[3],
-    state_to_search = sys.argv[4]
-    list_states(user, password, db, state_to_search)
+    state = sys.argv[4]
+    list_states(user, password, db, state)
