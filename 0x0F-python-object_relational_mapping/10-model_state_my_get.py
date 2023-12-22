f239ec4a2dded42c9b0d9dc9a472f0343b236cc7
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-22 05:04:32 +0000

    task 10

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Select states module """
+
+import sys
+import MySQLdb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+def list_states(username, password, db, state):
+    """ lists all states with a """
+    connection_url = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(username, password, db)
+    engine = create_engine(connection_url)
+
+    # step two - create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    all_states = session.query(State).order_by(State.id).all()
+    for state in all_states:
+        if  state.name == state:
+            print(state.id)
+            return
+    print("Not found")
+
+if __name__ == "__main__":
+    username, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    state = sys.argv[4]
+    list_states(username, password, db, state);
