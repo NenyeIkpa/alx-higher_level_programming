eb773a74e324c20acf29a201b588ca085229ac68
NenyeIkpa <nenyeikpa@gmail.com>
2023-12-22 06:05:41 +0000

    13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+""" Select states module """
+
+import sys
+import MySQLdb
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+def list_states(username, password, db):
+    """ lists all states """
+    connection_url = "mysql+mysqldb://{}:\
+            {}@localhost:3306/{}".format(username, password, db)
+    engine = create_engine(connection_url)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(
+            State.name.ilike("%a%")
+            ).delete(synchronize_session='fetch')
+    session.commit()
+
+
+if __name__ == "__main__":
+    username, password, db = sys.argv[1], sys.argv[2], sys.argv[3]
+    list_states(username, password, db)
