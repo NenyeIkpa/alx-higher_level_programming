28b4db70b519f969e360f0b3142470e71fa2becb
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-07 20:41:13 +0000

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+    fetches given url
+"""
+import urllib.request
+
+
+try:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        body = response.read()
+        bytes_to_string = body.decode('utf-8')
+        print("Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+        print("\t- utf8 content:", bytes_to_string)
+except Exception as e:
+    pass
