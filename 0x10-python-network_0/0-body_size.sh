6b34bc62239566df192688208d333ef766f82849
Chinenye Ikpa <nenyeikpa@gmail.com>
2024-01-07 09:05:22 -0800

    task 0 update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a request to a given url and displays the size of the body of the response
-curl $1 -X GET --write-out ${size_download} -s
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
