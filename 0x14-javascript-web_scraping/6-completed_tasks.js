5e2c94a69b8679835c1c5fadd78c4c5c40eda19a
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-30 21:30:38 +0000

    task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-readme.js <url>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+
+// display status code of request
+request.get(`${url}`, (err, res, body) => {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+  const completed = {};
+  const data = JSON.parse(body);
+  data.forEach((task) => {
+    if (task.completed) {
+      if (!completed[task.userId]) {
+        completed[task.userId] = 1;
+      } else {
+        completed[task.userId]++;
+      }
+    }
+  });
+  for (const userId in completed) {
+    console.log(`'${userId}': ${completed[userId]}`);
+  }
+});
