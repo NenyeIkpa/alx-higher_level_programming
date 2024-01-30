ae8e6a04055c8128b316cdb7806a050dce8282b5
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-30 21:39:45 +0000

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+if (process.argv.length !== 4) {
+  console.error('Usage: node script.js <URL> <FilePath>');
+  process.exit(1);
+}
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response && response.statusCode === 200) {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log(`Successfully saved the contents of ${url} to ${filePath}`);
+      }
+    });
+  } else {
+    console.error(`Error: Failed to retrieve content from ${url}.`);
+  }
+});
