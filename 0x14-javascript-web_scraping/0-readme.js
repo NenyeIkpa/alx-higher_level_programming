e29bbb2e76e24e9cf0c413e7e34ac962b80e99df
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-30 12:50:19 +0000

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: ./0-readme.js <file-path>');
+  process.exit(1);
+}
+
+const filePath = process.argv[2];
+
+// read file content in utf-8
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
